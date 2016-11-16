1ae282f82b455a2c5702604a261c7f1f3a3dd369
llgoody <934260425@qq.com>
2016-11-16 15:44:24 +0800

    generate order

--- app/Order.php
+++ app/Order.php
@@ -8,7 +8,23 @@ class Order extends Model
 {
     protected $table='order';
 
-    static function findGoods(){
-        
+    static function generateOrder($goodsId){
+        $user = \Auth::user();
+        if($user) {
+            $goods = \Goods::find($goodsId);
+            if ($goods) {
+                try {
+                    $order = new Order;
+                    $order->user_id = $user->id;
+                    $order->goods_id = $goodsId;
+                    $order->price = $goods->price;
+                    $order->save();
+                    return $order;
+                } catch (\Exception $e) {
+                    return 0;
+                }
+            }
+        }
+        return 0;
     }
 }
